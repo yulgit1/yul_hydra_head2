91151c6800d17ff7b2fc64a1b6a4136b8c0f67f3
root <root@infa7.(none)>
2013-01-23 19:32:00 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YulHydraHead2::Application.config.secret_token = '91e39efda4bc18de286f542908cd88d72be808ee2122a4efaf446e465027518773d4069c78206b0adce79e20c29d4bc413b7228891e05b7d752f36f6591714c0'
