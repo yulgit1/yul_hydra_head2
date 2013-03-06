4db8544d07c96e53907a9a7babbd94fb8ca69814
Eric James <eric.james@yale.edu>
2013-03-06 16:54:08 -0500

    added more terminology and indexing to simple_mods.rb

--- app/models/hydra/datastream/simple_mods.rb
+++ app/models/hydra/datastream/simple_mods.rb
@@ -14,11 +14,33 @@ module Hydra
       set_terminology do |t|
         t.root(:path=>"mods", :xmlns=>"http://www.loc.gov/mods/v3", :schema=>"http://www.loc.gov/standards/mods/v3/mods-3-2.xsd")
 
+		t.classification(:path=>"classification")
+		
+		t.name(:path=>"name") {
+		  t.namePart(:path=>"namePart") 
+		}  
         t.title_info(:path=>"titleInfo") {
           t.main_title(:path=>"title",:attributes=>{:type=>:none})
 		  t.alt_title(:path=>"title",:attributes=>{:type=>"alternative"})
-        } 
+        }
+        t.isbn(:path=>"identififer",:attributes=>{:type=>"isbn"}) 		
 	  end
+	  
+	  def extract_classifications
+        classifications = {}
+        self.find_by_terms(:classification).each do |aclass| 
+          ::Solrizer::Extractor.insert_solr_field_value(classifications, "classification_s", aclass.text) 
+        end
+        return classifications
+      end
+	  
+	  def extract_names
+        names = {}
+        self.find_by_terms(:name,:namePart).each do |name| 
+          ::Solrizer::Extractor.insert_solr_field_value(names, "names_t",name.text) 
+        end
+        return names
+      end
 	  
 	  def extract_titles
         titles = {}
@@ -36,11 +58,22 @@ module Hydra
         return titles
       end
 	  
+	  def extract_isbns
+        isbns = {}
+        self.find_by_terms(:isbn).each do |isbn| 
+          ::Solrizer::Extractor.insert_solr_field_value(isbns, "classification_s", isbn.text) 
+        end
+        return isbns
+      end
+	  
 	  def to_solr(solr_doc=Hash.new)
           super(solr_doc)
+		  solr_doc.merge!(extract_classifications)
+		  solr_doc.merge!(extract_names)
           solr_doc.merge!(extract_titles)
 		  solr_doc.merge!(extract_alt_titles)
-          solr_doc.merge!(:object_type_facet => "Generic content")
+		  solr_doc.merge!(extract_isbns)
+          solr_doc.merge!(:object_type_facet => "MODS described")
           solr_doc
         end
     end
